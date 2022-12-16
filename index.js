f91f24a18b0cc37424496f2d09dcceb9f1cfa4e7
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-16 22:29:16 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+const para = document.createElement("p");
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    })
+    .then(resp => resp.json())
+    .then((user) => {
+        console.log(user);
+        para.textContent = user.id;
+        document.body.appendChild(para);
+    })
+    .catch(e =>{
+        alert("Error!!");
+        para.textContent = e.message;
+        document.body.appendChild(para);
+    })
+} 
+
+submitData("mary", "mary@mary.com") 
